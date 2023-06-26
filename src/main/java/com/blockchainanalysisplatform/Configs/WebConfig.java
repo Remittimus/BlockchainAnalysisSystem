182b9bcf11d1e14afefa760798d8fd148ea11319
Remittimus <radis.cor1@gmail.com>
2023-06-26 20:44:27 +0300

    remove comments from previous version

--- src/main/java/com/blockchainanalysisplatform/Configs/WebConfig.java
+++ src/main/java/com/blockchainanalysisplatform/Configs/WebConfig.java
@@ -1,15 +1,19 @@
 package com.blockchainanalysisplatform.Configs;
 
+
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        //registry.addRedirectViewController("/","/checkSubscription");
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/instruction").setViewName("instruction");
     }
+
+
 }
